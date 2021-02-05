bc181a056b9a43b6fae3cd3829dd376ce192ea84
tai <ebh48978@gmail.com>
2021-02-05 18:09:19 +0900

    space作成,編集,削除ページ完了

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,14 +1,44 @@
 class SpacesController < ApplicationController
   before_action :authenticate_user!
   def new
+    @space = Space.new
   end
 
+  def create
+    @space = current_user.spaces.build(space_params)
+    if @space.save
+      redirect_to @space, notice:"スペースを掲載しました"
+    end
+  end 
+
   def index
+    @spaces = Space.all
   end
 
   def show
+    @space = Space.find_by(id: params[:id])
   end
 
   def edit
+    @space = Space.find_by(id: params[:id])
+  end
+  
+  def update
+    @space = Space.find_by(id: params[:id])
+    if @space.update(space_params)   
+      redirect_to @space, notice:"スペースの情報を更新しました"
+    end
+  end
+
+  def destroy
+    @space = Space.find_by(id: params[:id])
+    @space.delete
+    redirect_to spaces_path
+  end
+  
+  private
+
+  def space_params
+    params.require(:space).permit(:name, :pref, :city, :detailaddress, :tel, :station, :station_walk, :description)
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,10 +18,19 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+    #The path used after sign up.
+    def after_sign_up_path_for(resource)
+      spaces_path
+    end
+  
+    #The path used after sign up for inactive accounts.
+    def after_inactive_sign_up_path_for(resource)
+      root_path
+    end
 end
