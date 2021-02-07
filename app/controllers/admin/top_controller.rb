56f4b955dbd49a26f638449202a5492ab382573d
tai <ebh48978@gmail.com>
2021-02-07 11:48:11 +0900

    adminとユーザー画面の切り分け

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,4 +1,15 @@
 class Admin::TopController < ApplicationController
+
+  before_action :basic_auth    
+  protect_from_forgery with: :exception
+
   def index
   end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "hoge" && password == "hoge"
+    end
+  end
+  
 end
