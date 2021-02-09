956e9524e33c1b10fae81615dfbcb99d6c12db2d
tai <ebh48978@gmail.com>
2021-02-09 23:22:37 +0900

    uiの調整

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: 'AIzaSyALco9Ata7sUkQ5JE0RSz7Q0zqdF8cvvhQ', # API key for geocoding service
+  api_key: ENV['GOOGLE_MAP_API'], # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
