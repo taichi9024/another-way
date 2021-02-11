ec2de29badc943e27bd17d56432117abadc06c20
tai <ebh48978@gmail.com>
2021-02-11 15:13:22 +0900

    rspec導入 test削除

--- test/controllers/admin/manage_controller_test.rb
+++
@@ -1,8 +0,0 @@
-# require "test_helper"
-
-# class Admin::ManageControllerTest < ActionDispatch::IntegrationTest
-#   test "should get user" do
-#     get admin_manage_user_url
-#     assert_response :success
-#   end
-# end
--- test/controllers/profs_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ProfsControllerTest < ActionDispatch::IntegrationTest
-  test 'should get edit' do
-    get profs_edit_url
-    assert_response :success
-  end
-end
