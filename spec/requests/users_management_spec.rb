5fa46b17dc9028a5899ba5f785775e40a392f093
tai <ebh48978@gmail.com>
2021-02-18 22:54:13 +0900

    ユーザー新規登録に関するE2Eテストの追加

--- spec/requests/users_management_spec.rb
+++ spec/requests/users_management_spec.rb
@@ -1,12 +1,27 @@
 require 'rails_helper'
 
-describe 'Userの新規登録と更新' do
-  describe '新規登録' do
-    let(:params_hash) { attributes_for(:user) }
+feature 'users_management', type: :feature do
+  describe 'Userの新規登録と更新' do
+    describe '新規登録' do
+      example '新規登録成功、space一覧ページに遷移' do
+        visit signup_path
+        fill_in "user[email]", with: "hoge@hoge.com"
+        fill_in "user[name]" , with: "hoge"
+        fill_in "user[password]", with: "hogehoge"
+        fill_in "user[password_confirmation]", with: "hogehoge"
+        click_on "送信する"
+        expect(page).to have_selector 'h3', text: '物件一覧'
+      end
 
-    example '新規登録後スペーシ一覧ページに移動' do
-      post user_registration_path, params: { user: params_hash }
-      expect(response).to redirect_to(spaces_path)
+      example '新規登録失敗、再びsignupページへ遷移' do
+        visit signup_path
+        fill_in "user[email]", with: nil
+        fill_in "user[name]" , with: nil
+        fill_in "user[password]", with: nil
+        fill_in "user[password_confirmation]", with: nil
+        click_on "送信する"
+        expect(page).to have_selector 'h2', text: '新規登録'
+      end
     end
   end
 end
